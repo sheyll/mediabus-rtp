e20b93eb0358408a22462832a626992116f1ff88
Sven Heyll <sven.heyll@gmail.com>
2021-08-10 17:14:36 +0200

    Add new frame ring to demo program

--- mediabus-demo-rtp-alaw-player/Main.hs
+++ mediabus-demo-rtp-alaw-player/Main.hs
@@ -5,6 +5,7 @@ import           Conduit
 import           Data.MediaBus
 import           Control.Monad.Logger
 import           Data.MediaBus.Rtp
+import System.Exit (exitWith, ExitCode (ExitFailure))
 
 {- Send test data using the script in ./rtp-sender/send.sh.
  -
@@ -17,17 +18,33 @@ main :: IO ()
 main = do
     args <- getArgs
     case args of
-        [] -> mainASync
-        -- some extra arguments indicate that no ringbuffer shall be
-        -- put between sender and receiver.
-        (_ : _) -> mainSync
+        ("async-tbqueue" : _) -> mainASyncTB
+        ("async-framering" : _) -> mainASyncFR
+        ("sync" : _) -> mainSync
+        _ -> do
+          myself <- getProgName
+          putStrLn ""
+          putStrLn "Usage:"
+          putStrLn ""
+          putStrLn (myself ++ " async-tbqueue | async-framering | sync")
+          putStrLn ""
+          exitWith (ExitFailure 1)
 
-mainASync :: IO ()
-mainASync = runStdoutLoggingT $ runResourceT $
+mainASyncTB :: IO ()
+mainASyncTB = runStdoutLoggingT $ runResourceT $
     withAsyncPolledSource 20
                           (rtpAlaw16kHzS16Source 10000 "127.0.0.1" 5)
                           (\(_, !src) -> runConduit (src .|
                                                          exitAfterC maxFrames .|
+                                                         concealMissing .|
+                                                         debugAudioPlaybackSink))
+mainASyncFR :: IO ()
+mainASyncFR = runStdoutLoggingT $ runResourceT $
+    withConcurrentSource 20
+                          (rtpAlaw16kHzS16Source 10000 "127.0.0.1" 5 .| assumeSynchronizedC)
+                          (\(_, !src) -> runConduit (src .|
+                                                         exitAfterC maxFrames .|
+                                                         setSequenceNumberAndTimestampC @_ @(Hz 16000) @_ @RtpSeqNum  @Integer .|
                                                          concealMissing .|
                                                          debugAudioPlaybackSink))
 
